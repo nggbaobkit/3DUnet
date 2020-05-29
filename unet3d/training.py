fcddc63de3c7002daca2d893a3d0589c9e7dc208
Gia Bao <nggbaobkit@gmail.com>
2020-05-29 12:46:49 +0700

    Changed saving model strategy.

--- unet3d/training.py
+++ unet3d/training.py
@@ -22,7 +22,7 @@ def get_callbacks(model_file, initial_learning_rate=0.0001, learning_rate_drop=0
                   learning_rate_patience=50, logging_file="training.log", verbosity=1,
                   early_stopping_patience=None, model_best_path='checkpoints/model_best.h5'):
     callbacks = list()
-    callbacks.append(ModelCheckpoint(model_file, save_best_only=True))
+    callbacks.append(ModelCheckpoint(model_file, save_best_only=False, verbose=1))
     callbacks.append(CSVLogger(logging_file, append=True))
     if learning_rate_epochs:
         callbacks.append(LearningRateScheduler(partial(step_decay, initial_lrate=initial_learning_rate,
